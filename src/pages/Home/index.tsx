54789159458e7850a7ea32b443f97c43b2f2503d
mateus-s-silva <mattahyah@gmail.com>
2025-03-25 13:33:25 -0300

    feat: logica do contador

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import {
   ButtonContainer,
@@ -13,6 +13,7 @@ import {
   Separator,
   TaskInput,
 } from "./styles";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe o nome da tarefa"),
@@ -25,6 +26,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -46,15 +48,28 @@ export function Home() {
       id: new Date().getTime().toString(),
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(newCycle.id);
 
+    setAmountSecondsPassed(0);
+
     reset();
   }
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
